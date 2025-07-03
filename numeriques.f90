dbd7f973904b270ed4a552e19f12355f4858a220
Fatou Tine97 <92329317+FatouTine97@users.noreply.github.com>
2025-07-03 09:37:21 +0200

    Add files via upload

--- numeriques.f90
+++ numeriques.f90
@@ -5,5 +5,5 @@ real(dp) ,parameter ::  pi =acos(-1.0_dp)
  real(dp), parameter :: EPSILON_0 = 8.854187818d-12, MU_0 = 12.5663706144d-7
  real(dp), parameter :: fmax = 1e9
  real(dp), parameter :: c = 1.0d0 / (sqrt(EPSILON_0 * MU_0))
- integer,  parameter :: i1 = 200, j1 =0 , i2 =0, j2 = 0
+ integer,  parameter :: i1 = 200, j1 =0 
 end module lesconstantes_numeriques
