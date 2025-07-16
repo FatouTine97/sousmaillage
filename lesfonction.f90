289eef0efc22c4f5a4dfe92ecde107af824fb204
Fatou Tine97 <92329317+FatouTine97@users.noreply.github.com>
2025-07-16 16:32:22 +0200

    Add files via upload

--- lesfonction.f90
+++ lesfonction.f90
@@ -11,7 +11,7 @@ use lexcitation_donde
     contains
         procedure :: initialiser_champs
         procedure :: mise_a_jour_champs
-      !  procedure :: reference1
+       ! procedure :: reference1
     end type tableau
 
 contains
@@ -54,29 +54,8 @@ allocate(this%c_H(0:Nx, 0:Ny),  this%c_E(0:Nx, 0:Ny))
     end subroutine initialiser_champs
 
 
-subroutine couplage_CG_vers_FG(fd, i1, j1, r)
-  !  use lesconstantes_numeriques
-    implicit none
-    class(tableau), intent(inout) :: fd
-    integer, intent(in)           :: i1, j1, r
-    integer :: j, i, i_cg, j_cg
-
-    ! Injecte Hy CG → hy_s (bord gauche FG)
-    do j = 1, size(fd%hy_s, 2)-2
-        j_cg = j1 + j / r
-        fd%hy_s(0,j) = 0.5_dp * (fd%Hy(i1-1,j_cg) + fd%Hy(i1,j_cg))
-    end do
-
-    ! Injecte Hx CG → hx_s (bord bas FG)
-    do i = 1, size(fd%hx_s, 1)-2
-        i_cg = i1 + i / r
-        fd%hx_s(i,0) = 0.5_dp * (fd%Hx(i_cg,j1-1) + fd%Hx(i_cg,j1))
-    end do
-end subroutine couplage_CG_vers_FG
-
-
     ! Subroutine pour la mise à jour des champs
-    subroutine mise_a_jour_champs(fd, Nx, Ny, Nt, dx, dt, dy, Nx_sm, Ny_sm, Esrc)
+    subroutine mise_a_jour_champs(fd, Nx, Ny, Nt, n,dx, dt, dy, Nx_sm, Ny_sm, Esrc)
         use lesconstantes_numeriques
         implicit none
     class(tableau), intent(inout)                              :: fd
@@ -84,9 +63,9 @@ end subroutine couplage_CG_vers_FG
     real(dp), intent(in)                                       :: dx, dy, dt
     real(dp), intent(in)                                       :: Esrc(0:Nt-1)
     integer, parameter                                         :: r = 3
-    integer                                                    :: i, j, n,  m
+    integer                                                    :: i, j, n
     real(dp)                                                   :: Hz2, Hz3, dx_sm, dy_sm, dt_prime
-    real(dp)                                                   :: Haux_y, dx_prime, dy_prime, f_Hy, eaux_z, fez
+    real(dp)                                                   :: Haux_y, dx_prime, dy_prime, f_Hy, fez
     !real(dp)                                                   :: LCC, LIC1, LIC2, TCC1, TCC2, alpha 
     real(dp), dimension(0 : 1       , 0 : Ny)                  :: Ezx0_n1 , Ezx0_n2                    
     real(dp), dimension(Nx - 1 : Nx , 0 : Ny)                  :: Ezx_n1 , Ezx_n2                 
@@ -97,7 +76,8 @@ end subroutine couplage_CG_vers_FG
   !  real(dp), dimension(0 : 1       , 0 : Nx)                  :: Hy0_n1, Hy0_n2                    
   !  real(dp), dimension(Ny - 1 : Ny , 0 : Nx)                  :: Hy_n1, Hy_n2
     real(dp)                                                   :: coef_mur1, coef_mur2, coef_mur3
-   
+   real, allocatable :: eaux_z(:)
+   allocate(eaux_z(Ny_sm))
 
         dx_sm        = dx/ r
         dy_sm        = dy/ r
@@ -107,14 +87,8 @@ end subroutine couplage_CG_vers_FG
         fez          = 1.0
         dt_prime     = dt/r
 
-!ouverture des fichiers pour stockes les resultats
-open(unit=10, file="Ez_t.dat",        status = 'replace', action = 'write')
-open(unit=11, file="Hx_t.dat",        status = 'replace', action = 'write')
-open(unit=12, file="Hy_t.dat",        status = 'replace', action = 'write')
-open(unit=13, file="carto_t.dat",     status = 'replace', action = 'write')
-open(unit=14, file="carto_t1.dat",    status = 'replace', action = 'write')
-open(unit=15, file="ez_s.dat",        status = 'replace', action = 'write')
- m = 0
+
+
 Ezx0_n1 = 0.d0 ;    Ezx0_n2 = 0.0d0 
 Ezx_n1  = 0.d0 ;    Ezx_n2  = 0.0d0
 Ezy0_n1 = 0.d0 ;    Ezy0_n2 = 0.0d0
@@ -128,8 +102,7 @@ coef_mur1 = (c * dt - dx) / (c * dt + dx)
 coef_mur2 = (2.d0 * dx) / (c * dt + dx)
 coef_mur3 = (c * dt)**2 / ( 2 * dx * ( c * dt + dx ) )
 
-do n = 0, Nt - 1
-if (mod(n, 100) == 0) print *, "Itération temporelle n°", n
+
 ! Condition de bord x = 0
 Ezx0_n2        = Ezx0_n1                        ! temps n - 2 pas i = 0 et i = 1
 Ezx0_n1(0 , :) = fd%Ez(0,:)                     ! temps n - 1 pas i = 0
@@ -149,6 +122,7 @@ Ezy0_n1(1, :) = fd%Ez(:, 1)                     ! temps n - 1 pas j = 1
 Ezy_n2            = Ezy_n1                      ! temps n - 2 pas j = Ny - 1 et j = Ny
 Ezy_n1(Ny - 1, :) = fd%Ez(:, Ny - 1)            ! temps n - 1 pas j = Ny - 1
 Ezy_n1(Ny, :)     = fd%Ez(:, Ny)                ! temps n - 1 pas j = Ny
+
             
 do i = 1, Nx - 1
  if (i < i1 ) then
@@ -159,37 +133,17 @@ fd%Ez(i,j) = fd%Ez(i,j) + fd%c_E(i,j) * ((fd%Hy(i,j) - fd%Hy(i-1,j))/ dx  &
 end do
 end if
 end do
- 
-            !! Condition aux bord (Mur absorbant) x = 0
-fd%Ez(0 , :)  =  - Ezx0_n2(1, :)                                             &
-                 + coef_mur1 * (fd%Ez(1, :) + Ezx0_n2(0, :))                 &         ! n2 -> le temps n - 1    n1 -> n
-                  + coef_mur2 * (Ezx0_n1(0, :) + Ezx0_n1(1, :))
-
-            ! Condition aux bord (Mur absorbant) x = Nx
-fd%Ez(Nx , :) =    - Ezx_n2(Nx - 1, :)                                         &
-                  + coef_mur1 * (fd%Ez(Nx - 1, :) + Ezx_n2(Nx, :))             &
-                  + coef_mur2 * (Ezx_n1(Nx, :) + Ezx_n1(Nx - 1, :))
-
-            ! Condition aux bord (Mur absorbant) y = 0
-fd%Ez(0:i1 , 0)  =   - Ezy0_n2(1, :)                                             &
-                + coef_mur1 * (fd%Ez(:, 1) + Ezy0_n2(0, :))                      &
-                + coef_mur2 * (Ezy0_n1(0, :) + Ezy0_n1(1, :))
-
-!fd%Ez(i1:Nx, 0) = 0.0_dp 
-
-            ! Condition aux bord (Mur absorbant) y = Ny
-fd%Ez(0:i1 , Ny) =   - Ezy_n2(Ny - 1, :)                                         &
-                  + coef_mur1 * (fd%Ez(:, Ny - 1) + Ezy_n2(Ny, :))               &
-                  + coef_mur2 * (Ezy_n1(Ny, :) + Ezy_n1(Ny - 1,:))
-
-!fd%Ez(i1:Nx, Ny) = 0.0_dp ! Condition aux bord (Mur absorbant) pour le coin
+ fd%Ez(0,:) = 0.0_dp
+ fd%Ez(Nx,:) = 0.0_dp  
+fd%Ez(:,0) = 0.0_dp
+fd%Ez(:,Ny) = 0.0_dp
 
              ! Source dans domaine grossier
-fd%Ez(350,350) = fd%Ez(350, 350) + Esrc(n)
+fd%Ez(250,250) = fd%Ez(250, 250) + Esrc(n)
 
             ! Mise à jour Hx dans le domaine grossier       
 do i = 0, Nx - 1
- if (i <= i1 ) then
+ if (i < i1 ) then
 do j = 0, Ny - 1
 !if (j < j1 ) then
      fd%Hx(i,j) = fd%Hx(i,j) - fd%c_H(i,j) / dy * (fd%Ez(i,j+1) - fd%Ez(i,j))
@@ -216,11 +170,7 @@ do j = 1, Ny_sm- 1
     fd%hy_s(i-1,j))/dx_sm - (fd%hx_s(i,j) - fd%hx_s(i,j-1))/dy_sm)
    end do
   end do
-    ! Source dans le sous-maillage
-!if (mod(n, r) == 0) then
-   fd%ez_s(300,750) = fd%ez_s(300,750) + Esrc(n)
-!end if
-
+   
 
  !le champs magnetique(Hx) à l'intérieur du sous-maillage
 do i = 0, Nx_sm - 1
@@ -238,92 +188,76 @@ end do
 
 
 ! Interpolation depuis la grille fine 
-do j = 0, Ny_sm - 1
-     eaux_z = compute_ez_aux(fd, 0*r+1 , j)
-     end do 
+!do j = 0, Ny_sm - 1
 
-do i = 1, Nx - 1
-   do j = 1, Ny - 1
-   fd%Hy(i1, j) = fd%Hy(i1, j) + dt / (mu_0 * dx) * (fez*eaux_z- fd%Ez(i, j))
-   end do 
+     !eaux_z = compute_ez_aux(fd, 0*r+1 , j)
+   !  end do 
+do j = 0, Ny_sm - 1
+ eaux_z(j) = (1/3)*(1.0_dp/30_dp*  fd%ez(0-2,j) + 2.0_dp/30_dp*fd%ez(0-1, j) + fd%ez(0,j) + &
+  2.0_dp/30_dp* fd%ez(0+1, j) + 1.0_dp/30_dp* fd%ez(0+2, j) &
+         + 1.0_dp/30_dp* fd%ez(0, j-2) + 2.0_dp/30_dp* fd%ez(0, j-1) + 2.0_dp/30_dp* fd%ez(0, j+1) &
+         + 1.0_dp/30_dp* fd%ez(0, j+2))
+ enddo
+
+do i = 0, Nx - 1
+if (i < i1) then
+  do j = 0, Ny - 1
+   fd%Hy(i1, j) = fd%Hy(i1, j) + (fd%c_H(i,j)/dx) * (fez * eaux_z(j) - fd%Ez(i, j))
+  end do
+  end if 
 end do
+
 
 
 ! le champs electrique dans l'interface
 do j = 0, Ny - 1
 Hz2 = 0.0_dp
 Hz3 = 0.0_dp
-Hz2 = fd%Hy(i1-1, j)  
-Hz3 = fd%Hy(i1+1, j)
+Hz2 = fd%Hy(i1-2, j)  
+Hz3 = fd%Hy(i1-1, j)
 Haux_y = (2.0*Hz2 /9.0)+ (1.0*Hz3 /9.0)
 end do
 
+!print *, "Hz2 = ", Hz2, "Hz3 = ", Hz3
 
-do i =  1, Nx_sm - 1
-do j =  1, Ny_sm - 1
+do i =  0, Nx_sm - 1
+do j =  0, Ny_sm - 1
 fd%ez_s(0,j) = fd%ez_s(0,j) + (dt_prime/ epsilon_0 ) *((fd%hy_s(0,j) - (f_Hy *Haux_y))/dx_prime - &
               (fd%hx_s(i,j) - fd%hx_s(i,j-1))/dy_sm)
+       !       print*, "ez_s(0,j) = ", fd%ez_s(0,j)
 end do
 end do 
 
 
-    write(10,*) n*dt,       fd%Ez(350,250), fd%Ez(250,250), fd%Ez(150,250), fd%Ez(450,150)
-    write(11,*) n*dt,       fd%Hx(250,250), fd%Hx(150,100), fd%Hx(50,100), fd%Hx(100,150)
-    write(12,*) n*dt,       fd%Hy(250,250), fd%Hy(250,250), fd%Hy(50,100), fd%Hy(100,150)
-    write(15,*) n*dt_prime, fd%ez_s(300,750), fd%ez_s(450,750), fd%ez_s(500,600)
-
-   if (mod(n, 100) == 0 .and. n > 0) then
-    m = m + 1
-
-    !write(14,*) ! Ligne vide pour séparer les temps
-    ! on n’écrit que la valeur Ez(i,j), séparée par un espace
-    do i = 1, Nx, 2
-       write(14,*) (fd%Ez(i,j), j= 0, Ny, 2)
-    end do
-     write(14,*)
-
-     !valeur ez_s(i,j), séparée par un espace
-     do i = 1, Nx_sm, 2
-         write(13,*) (fd%ez_s(i,j), j= 1, Ny_sm, 2)
-    end do
- write(13,*)
-  end if   
-
- end do
-        print *, "Nombre de carto en temps = ", m
-        print *, "Fin de la simulation"
-        close(10); close(11); close(12); close(13); close(14); close(15)
-        close(16); close(17)
-   
  end subroutine mise_a_jour_champs
   
 
 
-  ! Interpolation conservatrice pour Ez aux interfaces FG → CG
-function compute_ez_aux(fd, i_f, j_f) result(ez_aux)
+ function compute_ez_aux(fd, i_f, j_f) result(ez_aux)
     use lesconstantes_numeriques
     implicit none
     type(tableau), intent(in) :: fd
-    integer, intent(in)       :: i_f, j_f  ! Indices dans l; close(14)a grille fine (FG)
+    integer, intent(in)       :: i_f, j_f
     real(dp)                  :: ez_aux
     real(dp) :: sum
     integer :: ii, jj
-   real(dp), dimension(-1:1, -1:1) :: w
+    real(dp), dimension(-1:1, -1:1) :: w
 
-    ! Pondération conservatrice 3x3 (somme = 16)
+    ! Pondération conservatrice 3x3, somme = 1
     w = reshape([ &
-        1.0/3.0_dp, 2.0/3.0_dp, 1.0_dp, 2.0/3.0_dp, 1.0/3.0_dp, &
-        2.0/3.0_dp, 1.0/3.0_dp, 1.0_dp, 1.0/3.0_dp ], [3,3])
-    ! Moyenne pondérée 3x3 centrée sur (i_f, j_f)
+        1.0_dp, 2.0_dp, 1.0_dp, &
+        2.0_dp, 1.0_dp, 2.0_dp, &
+        1.0_dp, 2.0_dp, 1.0_dp ], [3,3]) / 16.0_dp
+
     sum = 0.0_dp
     do jj = -1, 1
         do ii = -1, 1
-            sum = sum + w(ii, jj) * fd%ez_s(i_f, j_f+jj)
+            sum = sum + w(ii,jj) * fd%ez_s(i_f + ii, j_f + jj)
         end do
     end do
 
     ez_aux = sum
-    ez_aux = sum / 3.0_dp
 end function compute_ez_aux
+
 
 end module lesfonction  
--- main.f90
+++ main.f90
@@ -2,16 +2,18 @@ Program FDTD_sous_maillage
 use lesconstantes_numeriques
 use  lexcitation_donde
 use lesfonction
+!use lesreference
 implicit none
 ! Déclaration des variables
 type(tableau) :: sm
-integer :: Nx, Ny, Nt, Nx_sm, Ny_sm
+integer :: n, Nt, m , i, j
+integer :: Nx, Ny, Nx_sm, Ny_sm, Nx_r, Ny_r
 integer, parameter :: r = 3  ! facteur de raffinement du sous-maillage
 real(dp),dimension(:), allocatable :: Esrc
 real(dp), parameter :: dx = c / (fmax * 30.0d0)
 real(dp), parameter :: dy = c / (fmax * 30.0d0)
 real(dp), parameter :: dt = 0.98d0 / (c* sqrt(1/(dx*dx) + 1/(dy*dy)))
-
+real(dp), parameter :: dt_prime = dt /r
 
 Nx = 500
 Ny = 500
@@ -19,19 +21,67 @@ NT = 1200
  
 Nx_sm = (Nx - i1) *r
 Ny_sm = (Ny - j1) *r
+Nx_r = Nx*r
+Ny_r = Ny*r
 
-!print*, Nx_sm,Ny_sm
-
-
+print*, Nx_sm,Ny_sm, Nx_r, Ny_r
  
+
 ! Calcul de l'excitation
 call excitation_donde(Esrc, Nt, dt)
-    ! Initialiser les champs
-call sm%initialiser_champs(Nx, Ny, Nx_sm, Ny_sm, dt)
-call sm%mise_a_jour_champs(Nx, Ny, Nt, dx, dt, dy, Nx_sm, Ny_sm, Esrc)
-!call analyser_reflexion(fd, n, dt, Ez_ref, Ez_inc, "reflexion_log.dat")
 
+call sm%initialiser_champs(Nx, Ny, Nx_sm, Ny_sm, Nx_r,Ny_r, dt)
 
+  !ouverture des fichiers pour l'ecriture
+open(unit=10, file="GM/Ez_t.dat",           status = 'replace', action = 'write')
+open(unit=11, file="GM/Hx_t.dat",           status = 'replace', action = 'write')
+open(unit=12, file="GM/Hy_t.dat",           status = 'replace', action = 'write')
+open(unit=13, file="PM/carto_t.dat",        status = 'replace', action = 'write')
+open(unit=14, file="GM/carto_t1.dat",       status = 'replace', action = 'write')
+open(unit=15, file="PM/ez_s.dat",           status = 'replace', action = 'write')
+open(unit=16, file="R1/Ez_r1.dat",          status = 'replace', action = 'write')
+open(unit=17, file="R1/Hx_r1.dat",          status = 'replace', action = 'write')
+open(unit=18, file="R1/Hy_r1.dat",          status = 'replace', action = 'write')
+open(unit=19, file="R2/Ez_r2.dat",          status = 'replace', action = 'write')
+open(unit=20, file="R2/Hx_r2.dat",          status = 'replace', action = 'write')
+open(unit=21, file="R2/Hy_r2.dat",          status = 'replace', action = 'write')
+m =0
+do n = 0, Nt - 1
+if (mod(n, 100) == 0) print *, "Itération temporelle n°", n
+call sm%mise_a_jour_champs(Nx, Ny, Nt,n, dx, dt, dy, Nx_sm, Ny_sm, Esrc)
+!call reference1(Nx, Ny, Nt,n, dx, dt, dy, Esrc)
+!call reference2(sm, Nx_r, Ny_r,Nt, n, dx, dy, dt, Esrc)
+
+!Ecriture des champs dans les fichiers 
+write(10,*) n*dt,       sm%Ez(50,250),   sm%Ez(250,250),   sm%Ez(150,250), sm%Ez(250,150)
+write(11,*) n*dt,       sm%Hx(250,250),   sm%Hx(150,100),   sm%Hx(50,100), sm%Hx(100,150)
+write(12,*) n*dt,       sm%Hy(250,250),    sm%Hy(250,250),   sm%Hy(50,100), sm%Hy(100,150)
+write(15,*) n*dt_prime, sm%ez_s(300,750),  sm%ez_s(450,750), sm%ez_s(500,600)
+write(16,*) n*dt,       sm%Ez_r1(350,350), sm%Ez_r1(250,250), sm%Ez_r1(350,250), sm%Ez_r1(250,350)
+write(17,*) n*dt,       sm%Hx_r1(100,100), sm%Hx_r1(150,100), sm%Hx_r1(50,100), sm%Hx_r1(100,150)
+write(18,*) n*dt,       sm%Hy_r1(100,100), sm%Hy_r1(150,100), sm%Hy_r1(50,100), sm%Hy_r1(100,150)
+write(30,*) n*dt,       sm%Ez_r2(1050,1050), sm%Ez_r2(750,750), sm%Ez_r2(1050,750), sm%Ez_r2(750,1050)
+write(31,*) n*dt,       sm%Hx_r2(300,300),   sm%Hx_r2(150,300), sm%Hx_r2(50,100),    sm%Hx_r2(100,150)
+ write(32,*) n*dt,      sm%Hy_r2(100,100),  sm%Hy_r2(150,100), sm%Hy_r2(50,100),  sm%Hy_r2(100,150)
+
+   if (mod(n, 100) == 0 .and. n > 0) then
+    m = m + 1
+
+    !write(14,*) ! Ligne vide pour séparer les temps
+    ! on n’écrit que la valeur Ez(i,j), séparée par un espace
+    do i = 1, Nx, 2
+       write(14,*) (sm%Ez(i,j), j= 0, Ny, 2)
+    end do
+     write(14,*)
+
+     !valeur ez_s(i,j), séparée par un espace
+     do i = 1, Nx_sm, 2
+         write(13,*) (sm%ez_s(i,j), j= 1, Ny_sm, 2)
+    end do
+ write(13,*)
+  end if   
+
+ end do
     ! Libération de la mémoire
     deallocate(Esrc)
     deallocate(sm%Hx, sm%Ez, sm%Hy, sm%c_E, sm%c_H)
